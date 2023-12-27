fcb75fc6ae2498eaf3251a862945fb68f449b7e8
Teo Jimenez <teojimen@cbr15s4.42barcelona.com>
2023-12-27 22:30:32 +0100

    update 27/12

--- push_swap.c
+++ push_swap.c
@@ -11,33 +11,61 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "printf/ft_printf.h"
 
-int *atoi(char **argv, int *error)
+int atoi_error(char *str)
 {
+	int	i;
+	int	nb;
+	int	sig;
+
+	sig = 1;
+	i = 0;
+	nb = 0;
+	//look for something more
 	
+
+	if (str[i] == '-')
+	{
+		sig = sig * -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	return (nb * sig);
 }
 
-int	check_num(argc, argv)
+int	check_num(int argc, char **argv)
 {
-	int i = 0;
+	int i = 1;
 
-	int *argv_int;
+	// int *argv_int;
 	int *error = 0;
-	argv_int = (int *)malloc(argc - 1  * sizeof(int));
-	if (!argv_int)
-		return (0);
-	while (argc - 1 > 0)
+	// argv_int = (int *)malloc(argc - 1  * sizeof(int));
+	// if (!argv_int)
+	// 	return (-1); //error de asignar memoria
+	while (i < argc - 1)
 	{
-		argv_int = atoi(argv, &error);
-		if (error == -1)
-			return (-1); //hay algo que no es numero
+        if (atoi_error(argv[i]) == -1)
+        {
+            // free(argv_int);
+            return (-1);
+        }
+        // argv_int[i] = atoi_error(argv[i]);
+        i++;
 	}
+	// free(argv_int);
 	return (0);
 }
 
 int	main(int argc, char **argv)
 {
+	//put the numbers in a list
 	//check is all argv are numbers
-	check_num(argc, argv);
+	if(check_num(argc, argv) == -1)
+		ft_printf("ERROR");
 	return (0);
 }
