e6bf786e604fe3f8b4ad4d2fffde0813608f2f98
Teo Jimenez <teojimen@cbr1s3.42barcelona.com>
2024-02-04 16:27:55 +0100

    little change

--- chunks_size.c
+++ chunks_size.c
@@ -30,11 +30,25 @@ int	need_last(int nb, int *fractions, int chunk_size, int *last)
 	if (chunk_size * (*fractions) != nb)
 	{
 		*last = nb - (chunk_size * (*fractions));
-		(*fractions)++;
+		// (*fractions)++;
 		return (1);
 	}
 	else
 		return (0);
+}
+
+int sum_tab(int *tab)
+{
+	int tab_size;
+	int sum;
+	int i;
+
+	i = -1;
+	sum = 0;
+	tab_size = tabsize(tab);
+	while (tab_size > ++i)
+		sum += tab[i];
+	return (sum);
 }
 
 void	fill_result(int fractions, int **result, int c_size, int nb)
@@ -46,13 +60,16 @@ void	fill_result(int fractions, int **result, int c_size, int nb)
 	j = -1;
 	flag = need_last(nb, &fractions, c_size, &last);
 	while (++j < fractions)
-	{
-		if (j == fractions - 1 && flag == 1)
-			(*result)[j] = last;
-		else
 			(*result)[j] = c_size;
-	}
 	(*result)[fractions] = '\0';
+
+	j = -1;
+	while (sum_tab((*result)) < nb)
+	{
+		if(j - 1 == tabsize((*result)))
+			j = -1;
+		(*result)[++j]++;
+	}
 }
 
 int	*ft_fractions(int nb)
